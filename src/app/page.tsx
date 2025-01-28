33ff3b21cf09473d5dc8d304bfef9d208a99a184
Shreya Goyal <goyalshreya228@gmail.com>
2025-01-28 13:57:20 +0530

    small fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,19 @@
+"use server";
 import RoomBookingForm from "~/components/room-booking-form";
 import RoomGrid from "../components/room-grid";
 
-export default function Home() {
-
-//   <input
-//   type="number"
-//   onChange={(e)=>setValue(Number(e.target.value))}
-//   className="border rounded px-2 py-1"
-//   placeholder="Number of Rooms"
-// />
+export default async function Home() {
   return (
-  <>
-  <div className="space-y-5">
-  <div className="flex flex-col items-center gap-8">
-  <h1 className="text-3xl font-bold ">Hotel Room Reservation</h1>
-    <RoomBookingForm/>
+    <>
+      <div className="space-y-5">
+        <div className="flex flex-col items-center gap-8">
+          <h1 className="text-3xl font-bold">Hotel Room Reservation</h1>
+          <RoomBookingForm />
+        </div>
+        <div className="flex justify-center">
+          <RoomGrid />
+        </div>
       </div>
-      <div className="flex justify-center">
-      <RoomGrid />
-      </div>
-      </div>
-  </>
+    </>
   );
 }
